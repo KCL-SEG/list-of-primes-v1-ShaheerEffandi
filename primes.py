2db19d909aeac24e8c62c23d0a5b0c0b48c1e056
Shaheer Effandi <shaheereffandi@gmail.com>
2022-10-05 20:50:26 +0100

    Completed Exercise

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    index = 2
+    while len(list) < number_of_primes:
+        if len(list) == 0:
+            list.append(index)
+        else:
+            for prime in list:
+                if index % prime == 0:
+                    break
+            else:
+                list.append(index)
+        index += 1
     return list
